94d41700aa7d78633de54e62dbfd508525f326df
miragiee <miragiee564@gmail.com>
2026-05-06 15:09:25 +0300

    Сделал миграции таблиц БД

---
+++ database/migrations/2026_05_06_113351_create_documents_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('documents', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('student_internship_id')->constrained();
+            $table->text('file_path');
+            $table->string('type');
+            $table->timestamps('');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('documents');
+    }
+};
